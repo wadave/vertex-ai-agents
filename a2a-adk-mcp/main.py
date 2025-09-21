62ce370729057b3b34c4cb5677a99c89a33a8bc0
Dave Wang <wangdave@google.com>
2025-09-21 21:50:12 +0000

    remove folders

--- a2a-adk-mcp/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from a2a-demo!")
-
-
-if __name__ == "__main__":
-    main()
--- adk-remote-mcp/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from a2a-adk-mcp!")
-
-
-if __name__ == "__main__":
-    main()
